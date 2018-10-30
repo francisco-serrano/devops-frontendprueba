ce1101e5411d32469245042abe0f23aa61d44c84
Francisco <francisco.serrano372@gmail.com>
2018-10-30 11:18:12 -0300

    Adds button existence test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,24 +16,34 @@ describe('AppComponent', () => {
         RouterTestingModule,
         MatToolbarModule,
         MatCardModule,
-        MatButtonModule
+        MatButtonModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
   }));
+
+  it('chequea que esté el superbotón', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const button = element.querySelector('button');
+
+    expect(button).toBeDefined();
+  });
+
+  it(`should have as title 'frontend-prueba'`, () => {
+    const app = fixture.debugElement.componentInstance;
+
+    expect(app.title).toEqual('frontend-prueba');
+  });
 
   /* it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'frontend-prueba'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('frontend-prueba');
   });
 
   it('should render title in a h1 tag', () => {
